0e2c3d47a01f24381a7d62737015ef3da363efe1
max <r0609653@kuleuven.be>
2018-04-11 13:51:06 +0200

    fixed available item bug

--- app/src/main/java/com/example/max/testjson/AvailableItem.java
+++ app/src/main/java/com/example/max/testjson/AvailableItem.java
@@ -66,7 +66,8 @@ public class AvailableItem extends Item {
                 public void onSuccess(String result) {
                     Log.i("Success", "result----" + result);
                     try {
-
+                        availableItems = new ArrayList<AvailableItem>();
+                        availableItemMap = new HashMap<String, AvailableItem>();
                         JSONObject jsonObject = new JSONObject(result);
                         JSONArray jsonArray = jsonObject.getJSONArray("list");
                         for (int i = 0; i < jsonArray.length(); i++) {
