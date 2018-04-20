b114fd34007eb191d48fae13d503fdcd2f8c950c
max <r0609653@kuleuven.be>
2018-04-20 20:09:45 +0200

    admin overview fragment

--- app/src/main/java/com/example/max/testjson/Admin_AvailableItemFragment.java
+++ app/src/main/java/com/example/max/testjson/Admin_AvailableItemFragment.java
@@ -3,41 +3,41 @@ package com.example.max.testjson;
 import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Filter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.max.testjson.dummy.DummyContent;
 import com.example.max.testjson.dummy.DummyContent.DummyItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
-public class Admin_AvailableItemFragment extends Fragment {
+public class Admin_AvailableItemFragment extends Fragment implements SearchView.OnQueryTextListener{
 
-    // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
     private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
+    private AvailableItemFragment.OnListFragmentInteractionListener mListener;
+    private SearchView searchView;
+    private Spinner chooseLocation;
+    private AvailableItemRecyclerViewAdapter adapter;
+    private Toolbar toolbar;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public Admin_AvailableItemFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
     public static Admin_AvailableItemFragment newInstance(int columnCount) {
         Admin_AvailableItemFragment fragment = new Admin_AvailableItemFragment();
         Bundle args = new Bundle();
@@ -58,28 +58,40 @@ public class Admin_AvailableItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_item_list3, container, false);
+        View view = inflater.inflate(R.layout.fragment_availableitem_list, container, false);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS, mListener));
+        Context context = view.getContext();
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.available_list);
+        if (mColumnCount <= 1) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
         }
+
+        chooseLocation = (Spinner)view.findViewById(R.id.choose_location);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
+        chooseLocation.setAdapter(spinnerAdapter);
+
+        toolbar= (Toolbar)view.findViewById(R.id.toolbar);
+        searchView= (SearchView)view.findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(this);
+
+        adapter = new AvailableItemRecyclerViewAdapter(TestJson.getUser().availableItems, mListener);
+        recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(
+                getContext(), DividerItemDecoration.VERTICAL));
+
         return view;
     }
+
 
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
+        if (context instanceof AvailableItemFragment.OnListFragmentInteractionListener) {
+            mListener = (AvailableItemFragment.OnListFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnListFragmentInteractionListener");
@@ -92,18 +104,19 @@ public class Admin_AvailableItemFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(DummyItem item);
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
     }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        adapter.getFilter().filter(newText);
+        return false;
+    }
+
+    public interface OnListFragmentInteractionListener {
+        void onListFragmentInteraction(AvailableItem item);
+    }
+
 }
---
+++ app/src/main/java/com/example/max/testjson/Admin_AvailableItemRecycleViewAdapter.java
@@ -0,0 +1,159 @@
+package com.example.max.testjson;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import com.example.max.testjson.Admin_AvailableItemFragment.OnListFragmentInteractionListener;
+import com.example.max.testjson.dummy.DummyContent.DummyItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Admin_AvailableItemRecyclerViewAdapter extends RecyclerView.Adapter<Admin_AvailableItemRecyclerViewAdapter.ViewHolder> implements Filterable{
+
+    private List<AvailableItem> mValues;
+    private List<AvailableItem> tempValue;
+    private final AvailableItemFragment.OnListFragmentInteractionListener mListener;
+
+    AvailableItemRecyclerViewAdapter.TestFilter myFilter;
+
+    public Admin_AvailableItemRecyclerViewAdapter(List<AvailableItem> items, AvailableItemFragment.OnListFragmentInteractionListener listener) {
+        mValues = items;
+        tempValue = items;
+        mListener = listener;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_availableitem, parent, false);
+        return new Admin_AvailableItemRecyclerViewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final AvailableItemRecyclerViewAdapter.ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        String content = holder.mItem.getClassification() + " at "+holder.mItem.getItemLocation();
+        holder.mContent.setText(content);
+        if(((Student)TestJson.getUser()).inWishList(holder.mItem))
+            holder.mCheckBox.setChecked(true);
+        holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if ( isChecked )
+                {
+                    ((Student)TestJson.getUser()).addItemToWish(holder.mItem);
+                }
+                else
+                {
+                    ((Student)TestJson.getUser()).removeItemFromWish(holder.mItem);
+                }
+
+            }
+        });
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+
+    public AvailableItem getItem(int position){
+        return mValues.get(position);
+    }
+
+    @Override
+    public Admin_AvailableItemRecycleViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(Admin_AvailableItemRecycleViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mContent;
+        public AvailableItem mItem;
+        public CheckBox mCheckBox;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mContent = (TextView) view.findViewById(R.id.content);
+            mCheckBox = (CheckBox)view.findViewById(R.id.add_to_wish);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString()+mContent.getText();
+        }
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        if (myFilter == null) {
+            myFilter = new Admin_AvailableItemRecyclerViewAdapter.TestFilter();
+        }
+        return myFilter;
+    }
+
+    class TestFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+
+            List<AvailableItem> newItemList = new ArrayList<AvailableItem>();
+            if (constraint != null && constraint.toString().trim().length() > 0) {
+                for (int i = 0; i < tempValue.size(); i++) {
+                    String content = tempValue.get(i).getClassification() + " "+tempValue.get(i).getItemLocation();
+                    if (content.contains(constraint)) {
+                        newItemList.add(tempValue.get(i));
+
+                    }
+                }
+
+            }else {
+                newItemList = tempValue;
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.count = newItemList.size();
+            filterResults.values = newItemList;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mValues = (List)results.values;
+            if (results.count > 0) {
+                notifyDataSetChanged();
+            } else {
+                mValues = new ArrayList(){};
+                notifyDataSetChanged();
+            }
+
+        }
+
+    }
+
+}
